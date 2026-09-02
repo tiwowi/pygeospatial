562882165ccb1f64f2be87ebe0089064a51ae202
tomaszaba <tomas.zaba@outlook.com>
2026-09-02 17:11:49 +0200

    rename file name

---
+++ script.py
@@ -0,0 +1,7 @@
+## ---- Read in Geo files ------------------------------------------------------
+
+exec(open(file="scripts/reading_in_files.py").read())
+
+## ---- Working with geographical coordinate reference system ------------------
+
+exec(open(file="scripts/working_with_crs.py").read())
